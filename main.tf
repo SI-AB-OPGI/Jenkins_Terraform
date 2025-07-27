b03494212cfd0392bd919f8062f6915124c8b5f5
ABC-B11-GIABC-B11-GI <abhisheksag@yahoo.co.in>
2025-07-27 09:38:54 -0700

    TF Project

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource local_file file1 {
+   filename="file1"
+   content="local content1"
+  }
+
+resource local_file file2 {
+    filename="file2"
+    content="localcontent2"
+}
+resource local_file dir1 {
+    filename="dir1/ff"
+    content="this is dir1"
+}
+
+resource local_file dir2 {
+    filename="dir2/fg"
+    content="this is dir2"
+}
+
